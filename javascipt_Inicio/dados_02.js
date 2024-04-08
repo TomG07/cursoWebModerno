4043cd69580135f14b68e75f129bbb8b3a03bd3f
TomG <tomas.santos.frazao@gmail.com>
2024-04-08 18:58:41 +0100

    .

---
+++ javascipt_Inicio/dados_02.js
@@ -0,0 +1,7 @@
+let preco = 19.90
+let desconto = 0.4 // 40%
+
+console.log(preco * (1 - desconto)) // Preço depois do desconto
+
+let precoComDesconto = preco * (1 - desconto)
+console.log(precoComDesconto)
---
+++ javascipt_Inicio/dados_03.js
@@ -0,0 +1,12 @@
+let idade = 31
+console.log(typeof 31) // number
+console.log(typeof idade) // number
+
+let salario = 123.45
+console.log(typeof salario) // number
+
+let estaChovendo = true // or false
+console.log(typeof estaChovendo) // boolean
+
+console.log(typeof "Teste") // string
+console.log(typeof 'Teste') // string
